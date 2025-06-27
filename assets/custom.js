7d3f5e683650f7cac591d0de53e75c35b55305b6
Anupam 7Span <anupam@7span.com>
2025-06-27 09:26:27 +0530

    I have completed the Assignment with remaining Mobile layout and functionality.

--- assets/custom.js
+++ assets/custom.js
@@ -55,7 +55,46 @@ $(document).ready(function(){
     updateInventoryStatus();
   });
   
+  // Update on variant change
+$(document).on('change', 'variant-selects input[type="radio"]', function () {
+  setTimeout(function () {
+    const updatedPrice = $('.price-item--regular').first().text().trim();
+    $('.product-sticky-bar__price').text(updatedPrice);
+  }, 800); // Allow Shopify to fully update price (300ms is safe)
+});
+
+// Also run once on page load to sync price initially
+$(document).ready(function () {
+  const initialPrice = $('.price-item--regular').first().text().trim();
+  $('.product-sticky-bar__price').text(initialPrice);
+});
+
   
-  
+$(document).on('click', '.product-sticky-bar__button', function (e) {
+  e.preventDefault(); // prevent default anchor or button behavior
+  $('.product-form__submit').trigger('click');
+});
+
+
+$(document).scroll(function () {
+  var w = $(window);
+var offset = $(".custom-product-btn").offset();
+  var btnheight = parseFloat(offset.top-w.scrollTop());
+  console.log('btnheight-----', btnheight);
+  if (btnheight >= 900){  
+     if (window.matchMedia('(max-width: 991px)').matches)
+   {
+    $('.product_add_btn_fixed').fadeIn();
+         $('.product_add_btn_fixed').addClass('active');
+    }
+  }
+  else
+  {
+    $('.product_add_btn_fixed').fadeOut();
+    $('.product_add_btn_fixed').removeClass('active');
+  }
+});
+
+
   
  
